e645169f355bb61960c18f1b28c86ff3e7ffdd24
Bruno1234 <lucasjoaoponce@gmail.com>
2025-07-05 16:49:43 -0400

    DHCP

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
-      version = "0.7.1"  # pode ajustar para a versão que quiser
+      version = "0.7.1"
     }
     template = {
       source  = "hashicorp/template"
@@ -14,16 +14,30 @@ terraform {
 provider "libvirt" {
   uri = "qemu:///system"
 }
-#baixar imagem https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img
-resource "libvirt_volume" "ubuntu-qcow2" {
-  name   = "ubuntu-qcow2"
+
+resource "libvirt_volume" "ubuntu_base" {
+  name   = "ubuntu-base.qcow2"
   pool   = "default"
   source = "focal-server-cloudimg-amd64.img"
   format = "qcow2"
 }
 
+resource "libvirt_volume" "ubuntu_vm1" {
+  name           = "ubuntu-vm1.qcow2"
+  base_volume_id = libvirt_volume.ubuntu_base.id
+  pool           = "default"
+  format         = "qcow2"
+}
+
+resource "libvirt_volume" "ubuntu_vm2" {
+  name           = "ubuntu-vm2.qcow2"
+  base_volume_id = libvirt_volume.ubuntu_base.id
+  pool           = "default"
+  format         = "qcow2"
+}
+
 data "template_file" "user_data_vm1" {
-  template = file("${path.module}/cloud_init.cfg")
+  template = file("${path.module}/cloudinit/vm1.yaml")
 }
 
 resource "libvirt_cloudinit_disk" "cloudinit_vm1" {
@@ -32,17 +46,28 @@ resource "libvirt_cloudinit_disk" "cloudinit_vm1" {
   user_data = data.template_file.user_data_vm1.rendered
 }
 
+data "template_file" "user_data_vm2" {
+  template = file("${path.module}/cloudinit/vm2.yaml")
+}
+
+resource "libvirt_cloudinit_disk" "cloudinit_vm2" {
+  name      = "cloudinit-vm2.iso"
+  pool      = "default"
+  user_data = data.template_file.user_data_vm2.rendered
+}
+
 resource "libvirt_domain" "vm1" {
   name   = "vm1"
   memory = 1024
   vcpu   = 1
 
   disk {
-    volume_id = libvirt_volume.ubuntu-qcow2.id
+    volume_id = libvirt_volume.ubuntu_vm1.id
   }
 
   network_interface {
-    network_name = "default"
+    network_name = "custom-net"
+    mac          = "52:54:00:00:00:10"
   }
 
   console {
@@ -56,4 +81,30 @@ resource "libvirt_domain" "vm1" {
   }
 
   cloudinit = libvirt_cloudinit_disk.cloudinit_vm1.id
+}
+
+resource "libvirt_domain" "vm2" {
+  name   = "vm2"
+  memory = 1024
+  vcpu   = 1
+
+  disk {
+    volume_id = libvirt_volume.ubuntu_vm2.id
+  }
+
+  network_interface {
+    network_name = "custom-net"
+  }
+
+  console {
+    type        = "pty"
+    target_port = "0"
+  }
+
+  graphics {
+    type        = "spice"
+    listen_type = "none"
+  }
+
+  cloudinit = libvirt_cloudinit_disk.cloudinit_vm2.id
 }
